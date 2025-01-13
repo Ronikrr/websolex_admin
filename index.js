51f54da8565333104b5c2850eb344a3b55904b27
Ronik <102733678+Ronikrr@users.noreply.github.com>
2025-01-13 18:21:37 +0530

    webserver_2

--- index.js
+++ index.js
@@ -78,16 +78,7 @@ console.log(authenticate)
 app.use((err, req, res, next) => {
     res.status(500).json({ message: err.message });
 });
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/');
-    },
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, uniqueSuffix + path.extname(file.originalname));
-    },
-});
-const upload = multer({ storage });
+
 
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads/')));
