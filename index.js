5a9d4ff3c588af39b0638820ed901ba547838d07
Ronik <102733678+Ronikrr@users.noreply.github.com>
2025-01-17 11:46:05 +0530

    webserver_2

--- index.js
+++ index.js
@@ -1156,10 +1156,10 @@ app.get('/api/socialdetails', async (req, res) => {
 app.put('/api/socialdetails', async (req, res) => {
     console.log(req.body);
     try {
-        const { id, facebook, whatsapp, instagram, linkedin } = req.body;
+        const { _id, facebook, whatsapp, instagram, linkedin } = req.body;
 
         // Check if id is provided
-        if (!id) {
+        if (!_id) {
             return res.status(400).json({ message: 'ID is required for updating' });
         }
 
