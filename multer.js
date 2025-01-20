27f676bcb981fd0b85d316db4ecc0e61e72584bb
Ronik <102733678+Ronikrr@users.noreply.github.com>
2025-01-20 12:48:55 +0530

    20/1/2025

--- multer.js
+++ multer.js
@@ -1,17 +1,15 @@
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
-const cloudinary = require('./cloud'); // Import the Cloudinary configuration
+const cloudinary = require('./cloud'); 
 
-// Create a Cloudinary Storage instance
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
-        folder: 'profile_Image', // Specify the folder name in Cloudinary
-        allowed_formats: ['jpg', 'png', 'jpeg'], // Allowed file formats
+        folder: 'profile_Image',
+        allowed_formats: ['jpg', 'png', 'jpeg'], 
     },
 });
 
-// Initialize multer with Cloudinary storage
 const upload = multer({ storage: storage });
 
 module.exports = upload;
