4eea4d5bb03b59679befb4c02552948909294dbe
Ronik <102733678+Ronikrr@users.noreply.github.com>
2025-03-15 16:54:46 +0530

    last commnad

--- index.js
+++ index.js
@@ -356,6 +356,7 @@ app.put(
             if (email) updates.email = email;
             if (username) updates.username = username;
             if (phoneNo) updates.phoneNo = phoneNo;
+            if (workInCompany) updates.workInCompany = workInCompany;
             if (req.file) {
                 updates.profileImage = req.file.path;
             }
@@ -377,6 +378,7 @@ app.put(
                 email: updatedUser.email,
                 phoneNo: updatedUser.phoneNo,
                 profileImage: updatedUser.profileImage,
+                workInCompany: updatedUser.workInCompany,
             };
 
             res.json({
